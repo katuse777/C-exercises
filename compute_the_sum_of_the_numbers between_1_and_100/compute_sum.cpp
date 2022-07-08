2cbf3c5097baf56a535f95fedb4dc85503b14a14
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-08 17:03:46 +0200

    I've created  a new folder and this folder will host a program that when called will compute the sum of all the numbers between 1 and 100

---
+++ compute_the_sum_of_the_numbers between_1_and_100/compute_sum.cpp
@@ -0,0 +1,20 @@
+/*  compute the sum of the numbers between 1 and 100  */
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION 
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
