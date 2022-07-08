ad7ae8b8426ce5712ac76021cfdb2f73245e8dad
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-08 17:54:22 +0200

    I've added  a few functions that each have their specific job commented rigth next to them

--- Display_the_first_N__odd_numbers/display_odd_numbers.cpp
+++ Display_the_first_N__odd_numbers/display_odd_numbers.cpp
@@ -6,15 +6,42 @@
 #include <iostream>
 
 //FUNCTION PROTOTYPES
+unsigned number_of_odd_values(); // this function will take the value of N and use it to determine how odd numbers should be added together
+unsigned odd_numbers();  //this function will determine if a number is odd and if so it will be added to the sum
+unsigned sum_of_odd_values(unsigned); //this function will compute the sum of the first N odd numbers
+
 
 //GLOBAL VARIABLE DECLERATIONS
+
+
 
 //MAIN FUNCTION
 int main()
 {
+    std::cout << "Give me a positive number (N) and I will give you the sum of the first (N) odd numbers: ";
+    number_of_odd_values();
+
 
     system("pause>0");
     return 0;
 }
 
 //FUNCTION DEFINITIONS
+unsigned number_of_odd_values()
+{
+    unsigned N;
+    std::cin >> N;
+
+    return N;
+}
+
+unsigned sum_of_odd_values(unsigned N)
+{
+
+} 
+
+unsigned odd_numbers()
+{
+    int odd_num;
+    
+}
