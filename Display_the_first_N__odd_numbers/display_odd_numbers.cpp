9dad876c1cff1c959827b05f88ead3dacbba8354
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-08 17:31:40 +0200

    This folder will host a program that reads in a positive integer N and then calculates and   displays the sum of the first N odd integers

---
+++ Display_the_first_N__odd_numbers/display_odd_numbers.cpp
@@ -0,0 +1,20 @@
+/*Write a program that reads in a positive integer N and then calculates and
+  displays the sum of the first N odd integers. For example, if N is 4, your
+  program should display the value 16, which is 1 + 3 + 5 + 7.
+*/
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+//GLOBAL VARIABLE DECLERATIONS
+
+//MAIN FUNCTION
+int main()
+{
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
