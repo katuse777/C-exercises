029299409586fd05118c66c889979772f7edb625
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-09 12:51:53 +0200

    This program will read off of a list of numbers that represent the exam scores of students and then will print out the average

---
+++ calculate_the_average_marks/average.cpp
@@ -0,0 +1,25 @@
+/*write a program AverageList that reads in a list of integers representing exam scores
+  and then prints out the average. Because some unprepared student might
+  actually get a score of 0, your program should use −1 as the sentinel to mark the
+  end of the input.
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION 
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DECLERATIONS
