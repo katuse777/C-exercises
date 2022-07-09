9b40045203840dec4aac95955b6c832d6d1ca877
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-09 08:30:10 +0200

    The program does read a list of number but rigth now when i enter a number it gets printed  twice into the list and the largest value returned is always zero and this is not the expected outcome

--- find_the_largest_int/fiin_largest_int.cpp
+++ find_the_largest_int/fiin_largest_int.cpp
@@ -7,17 +7,53 @@
 //INCLUDE DIRECTIVES
 #include <iostream>
 
+
 //FUNCTION PROTOTYPES
+int get_user_input();
+int largest_num(int);  //this function returns the largest number
+
 
 //GLOBAL VARIABLE DECLERATIONS
+const int SENTINEL = 0;  // this variable holds the stop number
+
 
 //MAIN FUNCTION
 int main()
 {
+    std::cout << "Enter as many numbers as you want and Will return the largest number from the numbers you have entered.\n"
+    "TO STOP THE PROGRAM, ENTER THE NUMBER ZERO(0): ";
+    int absolute_max = get_user_input();
+    std::cout << "The largest number you have entered is: " << absolute_max << "\n";
 
 
     system("pause>0");
     return 0;
 }
 
+
 //FUNCTION DEFINITIONS
+int get_user_input()
+{
+    int user_num;  //this variable holds the entered by the  user and will be used to print list of values
+    int maximum_number;
+    do
+    {
+        std::cin >> user_num;
+        std::cout << user_num << "\n";
+        maximum_number = largest_num(user_num);
+
+    } while (user_num != SENTINEL);
+    
+    return maximum_number;
+}
+
+int largest_num(int user_num) 
+{
+    int max = 0;   //thsi variable will hold the largest number in the list
+
+    if (max < user_num)
+        max = user_num;
+
+
+    return max;     
+}
