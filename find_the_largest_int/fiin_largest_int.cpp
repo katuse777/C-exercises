48468a37d8e1cdf24fa9a5884610daddd288b6f3
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-09 07:42:25 +0200

    this folder will host a program that reads in a list of integers from the user until the user   enters the value 0 as a sentinel. When the sentinel appears, your program   should display the largest value in the list

---
+++ find_the_largest_int/fiin_largest_int.cpp
@@ -0,0 +1,23 @@
+/*Write a program that reads in a list of integers from the user until the user
+  enters the value 0 as a sentinel. When the sentinel appears, your program
+  should display the largest value in the list, as illustrated in the following sample
+  run:
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+//GLOBAL VARIABLE DECLERATIONS
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
