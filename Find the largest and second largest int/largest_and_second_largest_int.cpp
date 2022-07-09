547fcb3f50db327e7df839a45a54b521927907c0
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-09 12:21:38 +0200

    the program is able to find the largest number in the given list of numbers but it just assigns the last number entered before zero to the (second_largest) variable

--- Find the largest and second largest int/largest_and_second_largest_int.cpp
+++ Find the largest and second largest int/largest_and_second_largest_int.cpp
@@ -9,15 +9,21 @@
 
 
 //FUNCTION PROTOTYPES
-
+void find_largest_and_second_largest(int);  
+void connecting(); //this function will accept the numbers entered by the user and will call another function to process the numbers 
 
 //GLOBAL VARIABLE DECLERATIONS
+int largest = 0;
+int second_largest = 0;
 
 
 //MAIN FUNCTION
 int main()
 {
-
+    std::cout << "Give me a list of numbers and I will give you the two largest numbers in that list: \n";
+    connecting();
+    std::cout << "The Largest Number: " << largest << "\n";
+    std::cout << "The Second largest Number: " << second_largest << "\n";
 
     system("pause>0");
     return 0;
@@ -25,3 +31,23 @@ int main()
 
 
 //FUNCTION DEFINITIONS
+
+void connecting()
+{
+    int list_item;
+    do
+    {
+        std::cin >> list_item;
+        find_largest_and_second_largest(list_item);
+        
+    } while (list_item != 0);
+    
+}
+
+void find_largest_and_second_largest(int user_num)
+{
+    if (user_num > largest)
+        largest = user_num;
+    else if (user_num > second_largest)
+        second_largest = user_num;    
+}
