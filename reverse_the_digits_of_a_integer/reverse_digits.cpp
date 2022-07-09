b7e6becd0b1e272191f4aa15fdabe478cc1579e1
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-09 15:02:01 +0200

    This program will read in an integer and then display the number that has the same digits in the reverse order

---
+++ reverse_the_digits_of_a_integer/reverse_digits.cpp
@@ -0,0 +1,23 @@
+/*write a program that reads in an integer and then displays the
+  number that has the same digits in the reverse order
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+
+//FUNCTION DEFINITIONS
