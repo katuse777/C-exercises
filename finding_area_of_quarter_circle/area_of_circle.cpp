05ddeb5c11174e595925a3a6c7f432476c9df5fe
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-10 14:27:38 +0200

    This program will calculate the area of a circle by dividing the circle into 10000 rectangles

---
+++ finding_area_of_quarter_circle/area_of_circle.cpp
@@ -0,0 +1,24 @@
+/*Write a program to compute the area of the quarter circle by dividing it into
+  10,000 rectangles
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+#include <cmath>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE FUNCTIONS
+
+
+//MAIN FUNCTION 
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
