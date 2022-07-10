42451958df4e9e16054ea9d021f66eebd4dcf548
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-10 15:36:09 +0200

    The program is working as intended, the user can choose accurate they want the answer to be, by entering the number of rectangles

--- finding_area_of_quarter_circle/area_of_circle.cpp
+++ finding_area_of_quarter_circle/area_of_circle.cpp
@@ -7,14 +7,25 @@
 #include <cmath>
 
 //FUNCTION PROTOTYPES
-
+void calculating_width();
+void calculating_sum_of_area();
 
 //GLOBAL VARIABLE FUNCTIONS
+const double RADIUS = 2;
+double width;   //this variable will store the width of the rectangles
+int number_of_rectangles = 10000;
+double heigth; //this variable will store the heigth of the rectangles
+double sum_of_areas = 0;
 
 
 //MAIN FUNCTION 
 int main()
 {
+    std::cout << "I can find the area of a circle with a radius of 2 inches by dividing it into rectangles, the more rectangles I use the more accurate my answer will be.\n"
+    "Enter the number of rectangles you want: ";
+    std::cin >> number_of_rectangles;
+    calculating_sum_of_area();
+    std::cout << "Total area: " << sum_of_areas;
 
 
     system("pause>0");
@@ -22,3 +33,19 @@ int main()
 }
 
 //FUNCTION DEFINITIONS
+void calculating_width()
+{
+    width = RADIUS/number_of_rectangles;
+}
+
+void calculating_sum_of_area()
+{
+    calculating_width();
+    double midpoint = 0;
+    for (int i = 1; i < number_of_rectangles; i++)
+    {
+        midpoint += i*(midpoint/2);
+        heigth = sqrt((RADIUS*RADIUS)-(midpoint*midpoint));
+        sum_of_areas += heigth*width; 
+    }
+}
