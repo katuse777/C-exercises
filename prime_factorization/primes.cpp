e93b567962c685d2e0fdbe2f245abad3aa849958
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-10 08:28:45 +0200

    this program will display the prime factorization of a number n

---
+++ prime_factorization/primes.cpp
@@ -0,0 +1,21 @@
+/*Write a program to display the prime factorization of a number n*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION 
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
