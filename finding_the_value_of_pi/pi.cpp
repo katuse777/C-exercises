b871cd6ebd2123f10544d24ac510013a7c4a8141
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-10 12:11:30 +0200

    This program will approximate the value of pi/4

---
+++ finding_the_value_of_pi/pi.cpp
@@ -0,0 +1,21 @@
+/*Write a program that calculates an approximation of π consisting of the first
+  10,000 terms in Leibniz’s series
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+//GLOBAL VARIABLE DECLERATIONS
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
