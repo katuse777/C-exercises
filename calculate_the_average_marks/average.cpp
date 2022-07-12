657d4f540346bfc2fe6aad0fa2aaa5e422ccee08
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-12 11:24:18 +0200

    no changes have been made to this file

--- calculate_the_average_marks/average.cpp
+++ calculate_the_average_marks/average.cpp
@@ -1,57 +1,51 @@
-/*write a program AverageList that reads in a list of integers representing exam scores
-  and then prints out the average. Because some unprepared student might
-  actually get a score of 0, your program should use −1 as the sentinel to mark the
-  end of the input.
+/*Write a program to compute the area of the quarter circle by dividing it into
+  10,000 rectangles
 */
 
 //INCLUDE DIRECTIVES
 #include <iostream>
+#include <cmath>
 
 //FUNCTION PROTOTYPES
-void finding_the_average();  //this function will take user input and call another function to process the input
-void calculating_sum_and_number_of_scores(unsigned);  //this function will take the numbers entered by the user and find average
+void calculating_width();
+void calculating_sum_of_area();
 
-//GLOBAL VARIABLE DECLERATIONS
-unsigned sum = 0;
-unsigned number_of_scores = 0;
-unsigned average;
-const int SENTINEL = -1;
+//GLOBAL VARIABLE FUNCTIONS
+const int RADIUS = 2;
+double width;   //this variable will store the width of the rectangles
+int number_of_rectangles = 10000;
+double heigth; //this variable will store the heigth of the rectangles
+double sum_of_areas = 0;
+
 
 //MAIN FUNCTION 
 int main()
 {
-    std::cout << "Enter the exam scores of all your students and I'll give you their average: \n";
-    std::cout << "Enter a -1 to terminate the program and get your results:\n";
-    finding_the_average();
-    std::cout << "The average of all students is: " << average << "\n";
+    std::cout << "I can find the area of a circle with a radius of 2 inches by dividing it into rectangles, the more rectangles I use the more accurate my answer will be.\n"
+        "Enter the number of rectangles you want: ";
+    std::cin >> number_of_rectangles;
+    calculating_sum_of_area();
+    std::cout << "Total area: " << sum_of_areas;
 
 
     system("pause>0");
     return 0;
 }
 
-//FUNCTION DECLERATIONS
-void finding_the_average()
+//FUNCTION DEFINITIONS
+void calculating_width()
 {
-    unsigned user_num;
-
-    do
-    {
-        std::cin >> user_num;
-        if (user_num == SENTINEL)
-            break;
-        calculating_sum_and_number_of_scores(user_num);
-
-    } while (true);
-    
-    if (number_of_scores > 0)
-        average = sum / number_of_scores;
-    else 
-        std::cout << "Try actually entering number's next time\n";    
+    width = RADIUS / number_of_rectangles;
 }
 
-void calculating_sum_and_number_of_scores(unsigned user_num)
+void calculating_sum_of_area()
 {
-    sum += user_num;
-    ++number_of_scores;
+    calculating_width();
+    double midpoint = 0;
+    for (int i = 1; i < number_of_rectangles; i++)
+    {
+        midpoint += i * (midpoint / 2);
+        heigth = sqrt((RADIUS * RADIUS) - (midpoint * midpoint));
+        sum_of_areas += heigth * width;
+    }
 }
