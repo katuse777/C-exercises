0ddecbb6660284f8bd51250df6b66183a823e21d
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-13 09:13:40 +0200

    The program is working as intended, the program only excepts a floating point number and correctly rounds it off to the nearest integer

--- rounding/rounding.cpp
+++ rounding/rounding.cpp
@@ -7,17 +7,26 @@
 #include <iostream>
 
 //FUNCTION PROTOTYPES
-
+int round_to_nearest_int(float); //this function will take in a floating point number and round it off to hte nearest integer
 
 //GLOVBAL VARIABLE DECLERATIONS
-
+float user_number;
 
 //MAIN FUNCTION
 int main()
 {
+    std::cout << "Enter a floating point number and I will round it off to the nearest integer: ";
+    std::cin >> user_number;
+    std::cout << round_to_nearest_int(user_number);
+
     system("pause>0");
     return 0;    
 }
 
 
 //FUNCTION DEFINITIONS
+int round_to_nearest_int(float num)
+{
+    num += 0.5;
+    return num;
+}
