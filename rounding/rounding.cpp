0d7e90599e410bf92fbf5046b16d729d7304b899
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-13 09:02:39 +0200

    This program will take a floating point number and round it off to the nearest integer

---
+++ rounding/rounding.cpp
@@ -0,0 +1,23 @@
+/*Write a function roundToNearestInt(x) that rounds the floating-point
+  number x to the nearest integer. Show that your function works by writing a
+  suitable main program to test it.
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOVBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION
+int main()
+{
+    system("pause>0");
+    return 0;    
+}
+
+
+//FUNCTION DEFINITIONS
