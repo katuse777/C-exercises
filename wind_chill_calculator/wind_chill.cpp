9fa1013bf261e2aaef95ab7f18c2173160460e96
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-13 09:25:49 +0200

    This program will accept a tempreature in fahrenheit and a wind speed in miles per hour and will return the wind chill

---
+++ wind_chill_calculator/wind_chill.cpp
@@ -0,0 +1,29 @@
+/*Write a function windChill that takes the values of t and v and returns the
+  wind chill. In doing so, your function should take account of two special cases:
+  • If there is no wind, windChill should return the original temperature t.
+  • If the temperature is greater than 40° F, the wind chill is undefined, and
+  your function should call error with an appropriate message.
+
+  35.74 + 0.6215 t – 35.75 v^0.16 + 0.4275 t v^0.16
+  where t is the Fahrenheit temperature and v is the wind speed in miles per hour
+*/
+
+//INCLUDE DIRECTIVES
+#include <iostream>
+
+//FUNCTION PROTOTYPES
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
+
+//FUNCTION DEFINITIONS
