f62d50583b4081caf0147cde5334b143a9d219ba
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-13 15:03:14 +0200

    The program is working as planned, now the program will return the very same temperature it received if the wind speed is zero and wind chill is undefined for temperatures higher than 40

--- wind_chill_calculator/wind_chill.cpp
+++ wind_chill_calculator/wind_chill.cpp
@@ -11,12 +11,14 @@
 //INCLUDE DIRECTIVES
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 //FUNCTION PROTOTYPES
-void get_wind_speed_and_temp(); 
-double calculate_wind_chill(double & wind_s, double & temperature); 
-void print_wind_chill(double wind_chill);
-void wind_to_the_Nth_pow(double & wind);
+void get_wind_speed_and_temp();
+double calculate_wind_chill(double& wind_s, double& temperature);
+void print_wind_chill(int wind_chill);
+void wind_to_the_Nth_pow(double& wind);
+void error(std::string);
 
 //GLOBAL VARIABLE DECLERATIONS
 
@@ -32,30 +34,54 @@ int main()
 }
 
 //FUNCTION DEFINITIONS
+
+/*get_wind_speed_and_temp function
+  --------------------------------
+  this function will ask the user to enter the current temperature and wind speed
+  and it will call other functions to which it will pass these valuse into for
+  processing and call another function which will use that return value to print
+  the wind chill to the user
+*/
 void get_wind_speed_and_temp()
 {
     double wind_speed, temp;
-    std::cout << "Enter the temperature: ";
+    std::cout << "Enter the temperature(degrees Fahrenheit): ";
     std::cin >> temp;
-    std::cout << "Enter the wind speed: ";
+    std::cout << "Enter the wind speed(miles per hour): ";
     std::cin >> wind_speed;
 
-    double wind_chill = calculate_wind_chill(wind_speed, temp);
+    int wind_chill = calculate_wind_chill(wind_speed, temp);
     print_wind_chill(wind_chill);
 }
 
-double calculate_wind_chill(double & wind_s, double & temperature)
-{
-    wind_to_the_Nth_pow(wind_s);
-    return 35.74 + (0.6215 * temperature) - (35.75 * wind_s) + (0.4275 * temperature * wind_s);
-}
 
-void wind_to_the_Nth_pow(double & wind)
+/* wind_to_the_Nth_pow function
+   ----------------------------
+   this funtion will be used to simplify the calculation of the wind chill, specifically, it calculates
+   (v^0.16) the wind speed to the power 0.16
+*/
+
+void wind_to_the_Nth_pow(double& wind)
 {
     wind = pow(wind, 0.16);
 }
 
-void print_wind_chill(double wind_chill)
+double calculate_wind_chill(double& wind_s, double& temperature)
 {
-    std::cout << "Wind Chill: " << wind_chill;
+    if (temperature > 40.0) error("Undefined");
+    if (wind_s == 0.0) return temperature;
+    wind_to_the_Nth_pow(wind_s);
+    return 35.74 + (0.6215 * temperature) - (35.75 * wind_s) + (0.4275 * temperature * wind_s);
+}
+
+
+void print_wind_chill(int wind_chill)
+{
+    std::cout << "Wind Chill: " << wind_chill << " degrees Fahrenheit.";
+}
+
+void error(std::string msg)
+{
+    std::cerr << msg << "\n";
+    exit(EXIT_FAILURE);
 }
