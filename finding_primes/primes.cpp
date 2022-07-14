4a5831119766510a92a67abbbecce3b993754dec
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-14 13:04:44 +0200

    This program should take a number, determine whether or not that number is a prime and if so, print it to the console

---
+++ finding_primes/primes.cpp
@@ -0,0 +1,15 @@
+/*Write a predicate
+  method isPrime(n) that returns true if the integer n is prime, and false
+  otherwise. To test your algorithm, write a main program that lists the prime
+  numbers between 1 and 100.
+*/
+
+#include <iostream>
+
+
+int main()
+{
+    
+    system("pause>0");
+    return 0;
+}
