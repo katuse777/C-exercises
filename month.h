6acc63b641f342fd8be26473856999b20f8fe92e
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-15 14:49:06 +0200

    The month.h header file now contains all the prototypes of the functions it will be exporting and the MONTH data type has been defined

---
+++ month.h
@@ -0,0 +1,36 @@
+#ifndef _month_h
+#define _month_h
+
+enum MONTH 
+{
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+
+/*bool leap_year(int year)
+  -----------------------
+  this function returns true if the year passed into it is indeed a leap year and false otherwise
+*/
+bool leap_year(int year);
+
+
+
+/*int days_in_month(int year, int month)
+  --------------------------------------
+  takes in a year and a month, if the year passed in is a leap year and returns the number of days
+  that month should have
+*/
+int days_in_month(int year, int month);
+
+#endif
