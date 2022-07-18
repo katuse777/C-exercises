84db64634cf0bf39614c5b15cc92861945e0f77a
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-18 08:43:22 +0200

    The program will take a string and check if it ends with a given expression and if so, return true

---
+++ endsWith/endsWith.cpp
@@ -0,0 +1,21 @@
+/*Implement the function endsWith(str, suffix), which returns true if
+  str ends with suffix. Like its startsWith counterpart, the endsWith
+  function should allow the second argument to be either a string or a character
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
