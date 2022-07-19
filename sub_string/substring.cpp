50a635271fe8120d19380b136d2be468b226ae1b
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-19 17:24:30 +0200

    This program contains the free function version of the substr method from the string library, it works by accepting atleast 2 arguments, the string to extract from and the index to start extracting from and a third optional argument which specifies the number of characters to extract

--- sub_string/substring.cpp
+++ sub_string/substring.cpp
@@ -15,7 +15,7 @@
 
 //FUNCTION PROTOTYPES
 std::string substr(std::string,int, int);
-
+std::string substr(std::string ,int);
 
 std::string user_string;
 int position, number_of_characters;
@@ -29,18 +29,45 @@ int main()
     std::cout << "How many characters to extract: ";
     std::cin >> number_of_characters;
     std::string new_string;
-    new_string = substr(user_string, position, number_of_characters);
-    std::cout << "New string: " << new_string;
+    new_string = substr(user_string, position);
+    std::cout << "New string: " << new_string << '\n';
 
 
     system("pause>0");
     return 0;
 }
 
+/*std::string substr(std::string str,int pos,int n)
+  -------------------------------------------------
+  this function returns a substring of a string passed in as the first arguement and
+  the pos parameter excepts an integer that specifies the index at which the extraction
+  should start and the integer n specifies the number of character to be extracted
+*/
 std::string substr(std::string str,int pos,int n)
 {
     std::string sub_string = "";    
     for (int i = 1; i <= n; i++)
+    {
+        sub_string += str[pos];
+        pos++;
+    }
+    return sub_string;
+}
+
+
+/*std::string substr(std::string str,int pos)
+  -------------------------------------------
+  this is the overloaded version of the std::string substr(std::string str,int pos,int n) function
+
+  this function returns a substring of a string passed in as the first arguement and
+  the pos parameter excepts an integer that specifies the index at which the extraction
+  should start and if the third argument is left out, the remainder of the string will
+  be extracted after the starting position
+*/
+std::string substr(std::string str,int pos)
+{
+    std::string sub_string = "";    
+    for (int i = 1; i <= str.length(); i++)
     {
         sub_string += str[pos];
         pos++;
