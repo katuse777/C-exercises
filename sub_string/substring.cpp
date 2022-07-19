3d87e9d08f63aae50f82d3c2c236a0577bb716a3
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-19 17:08:54 +0200

    This program is a free function version of the substr method from the string library but this function needs Three arguements to function

--- sub_string/substring.cpp
+++ sub_string/substring.cpp
@@ -14,15 +14,36 @@
 
 
 //FUNCTION PROTOTYPES
+std::string substr(std::string,int, int);
 
 
-
+std::string user_string;
+int position, number_of_characters;
 //MAIN FUNCTION
 int main()
 {
-
+    std::cout << "Enter a string: ";
+    getline(std::cin, user_string);
+    std::cout << "Now I can extract a piece of that string, enter the starting position for the extraction: ";
+    std::cin >> position;
+    std::cout << "How many characters to extract: ";
+    std::cin >> number_of_characters;
+    std::string new_string;
+    new_string = substr(user_string, position, number_of_characters);
+    std::cout << "New string: " << new_string;
 
 
     system("pause>0");
     return 0;
+}
+
+std::string substr(std::string str,int pos,int n)
+{
+    std::string sub_string = "";    
+    for (int i = 1; i <= n; i++)
+    {
+        sub_string += str[pos];
+        pos++;
+    }
+    return sub_string;
 }
