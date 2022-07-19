ed118b894e52b11982cd8d775bc40f59d6e08f1b
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-19 11:48:56 +0200

    This program will contain a function the returns a sub string of a given number of characters from a string given by the user

---
+++ sub_string/substring.cpp
@@ -0,0 +1,28 @@
+/*Without using the built-in string method substr, implement a free function
+  substr(str, pos, n) that returns the substring of str beginning at position
+  pos and containing at most n characters. Make sure that your function
+  correctly applies the following rules:
+  • If n is missing or greater than the length of the string, the substring should
+  extend through the end of the original string.
+  • If pos is greater than the length of the string, substr should call error
+  with an appropriate message.
+*/
+
+#include <iostream>
+#include <string>
+
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    system("pause>0");
+    return 0;
+}
