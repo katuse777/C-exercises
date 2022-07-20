b10bc58073e6efc0dd8318b9b11ae5d84f243979
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-20 14:30:08 +0200

    This program will read words from the console and print out the score in scrabble

---
+++ scrabble_points/scrabble.cpp
@@ -0,0 +1,21 @@
+/*Write a program that reads in words
+  and prints out their score in Scrabble, not counting any of the other bonuses
+  that occur in the game. You should ignore any characters other than uppercase
+  letters in computing the score. In particular, lowercase letters are assumed to
+  represent blank tiles, which can stand for any letter but have a score of 0.
+*/
+
+#include <iostream>
+#include <string>
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
