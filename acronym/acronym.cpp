8e6f4948998896dfa4cd3d270dc575a47d5b6c7e
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-20 15:43:28 +0200

    This program will take in a string and return it's acronym

---
+++ acronym/acronym.cpp
@@ -0,0 +1,20 @@
+/*Write a function acronym that takes a string and returns the acronym formed from
+  that string. To ensure that your function treats hyphenated compounds like
+  self-contained as two words, it should define the beginning of a word as any
+  alphabetic character that appears either at the beginning of the string or after a
+  nonalphabetic character.
+*/
+
+#include <iostream>
+#include <string>
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+    system("pause>0");
+    return 0;
+}
