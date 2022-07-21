230ff37a39c2ab5c3f39ca2df6c1a61fad446641
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 17:32:48 +0200

    This program will contain a function that accepts three arguements,(string str,char c1,char c2) and will return str with every instance of c1 replaced with c2

---
+++ replace_ch/replace_ch.cpp
@@ -0,0 +1,20 @@
+/*Write a function, string replaceAll(string str, char c1, char c2);
+  that returns a copy of str with every occurrence of c1 replaced by c2. For
+  example, calling replaceAll("nannies", 'n', 'd');
+  should return "daddies".
+*/
+
+#include <iostream>
+#include <string>
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
