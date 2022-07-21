a2c235fe45832d7ba68a21bd6bea4b4bc997dd7a
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 17:24:59 +0200

    This program will accept a string from  the user and return the string with a version of itself where any two letter that appear next to each other and are the same character then they will replaced with only one of that character

--- remove_double_letters/remove_double_letters.cpp
+++ remove_double_letters/remove_double_letters.cpp
@@ -7,13 +7,36 @@
 
 
 //FUNCTION PROTOTYPES
+std::string remove_double_letters(std::string & str);
 
 
+
+
+std::string user_string;
 //MAIN FUNCTION
 int main()
 {
-
+    std::cout << "Enter a string with words that containletters that appear twice and next to each other: ";
+    getline(std::cin, user_string);
+    std::cout << remove_double_letters(user_string);
 
     system("pause>0");
     return 0;
+}
+
+/*std::string remove_double_letters(std::string & str)
+  ----------------------------------------------------
+  this function loops through the string passed in and checks if the current character is the same as the character
+  that came before it and if so, deletes the previous character 
+*/
+std::string remove_double_letters(std::string & str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == str[i - 1])
+        {
+            str.erase(i - 1, 1);
+        }
+    }
+    return str;
 }
