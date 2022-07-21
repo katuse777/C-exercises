83b7ba6553bac575327550c53d7db0566e55b659
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 21:35:03 +0200

    This program will identify strings that are given by the user as palindromes or not

---
+++ palindromes/palindrome.cpp
@@ -0,0 +1,20 @@
+/*Write a predicate function isSentencePalindrome(str) that returns
+  true if the string str fits this definition of a sentence palindrome
+*/
+
+#include <iostream>
+#include <string>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
