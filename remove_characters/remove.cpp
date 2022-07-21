22bb84e56254d719bed353df0a13c6cf443469fe
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 17:05:14 +0200

    This program accepts a string from the user and then asks the user which characters should be removed the given string and returns that string without the specified characters

--- remove_characters/remove.cpp
+++ remove_characters/remove.cpp
@@ -12,13 +12,44 @@
 
 
 //FUNCTION PROTOTYPES
+std::string remove_characters(std::string & str, std::string remove);
 
 
-
+std::string user_string, remove_ch;
+//MAIN FUNCTION
 int main()
 {
+    std::cout << "Enter a string: ";
+    getline(std::cin, user_string);
+    std::cout << "Enter a string all the character's you'd like me to remove from your string: ";
+    std::cin >> remove_ch;
+    std::cout << remove_characters(user_string, remove_ch);
 
 
     system("pause>0)");
     return 0;
+}
+
+
+/*std::string remove_characters(std::string & str, std::string remove)
+  --------------------------------------------------------------------
+  this function loops through the str, given by the user and compares
+  each character of the string with each character of the string 'remove'
+  and if the character in str match a letter in 'remove', that letter 
+  is deleted from str and returns str
+*/
+std::string remove_characters(std::string & str, std::string remove)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < remove.length(); j++)
+        {
+            if (str[i] == remove[j])
+            {
+                str.erase(i,1);
+                j = 0;
+            }
+        }
+    }
+    return str;
 }
