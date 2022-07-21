047d68fcf04c32b67a636d4da17f5a6758ea590c
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 15:56:07 +0200

    This program will contain a function that removes user specified characters from a user given string

---
+++ remove_characters/remove.cpp
@@ -0,0 +1,24 @@
+/*Write a function, string removeCharacters(string str, string remove);
+  that returns a new string consisting of the characters in str after removing all
+  instances of the characters in remove. For example, if you call
+  removeCharacters("counterrevolutionaries", "aeiou")
+  the function should return "cntrrvltnrs", which is the original string after
+  removing all of its vowels.
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+int main()
+{
+
+
+    system("pause>0)");
+    return 0;
+}
