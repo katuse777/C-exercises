4c66b3c72697c14f2badf8469fdf653c89790371
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-21 17:15:56 +0200

    This program will accept a string from  the user and return the string with a version of itself where any two letter that appear next to each other and are the same character then they will replaced with only one of that character

---
+++ remove_double_letters/remove_double_letters.cpp
@@ -0,0 +1,19 @@
+/*Write a function removeDoubledLetters(str) that returns a new string in
+  which any duplicated characters in str have been replaced by a single copy
+*/
+
+#include <iostream>
+#include <string>
+
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
