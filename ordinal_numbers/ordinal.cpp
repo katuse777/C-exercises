cf87b9cdf6c2dcadfb4aaab28795494a58174724
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-22 14:11:17 +0200

    This program will contain a function that takes in a number and returns that number as a string in ordinal form

---
+++ ordinal_numbers/ordinal.cpp
@@ -0,0 +1,20 @@
+/*Devise a rule that determines what suffix should be added to each number, and then use this rule
+  to write a function createOrdinalForm(n) that returns the ordinal form of
+  the number n as a string.
+*/
+
+#include <iostream>
+#include <string>
+
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
