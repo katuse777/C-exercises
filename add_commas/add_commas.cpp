3c36d6476e247675a8fadcba603fea37dbbb68d2
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-22 21:24:55 +0200

    This program will take a number and return that number as a string with commas at every third position

---
+++ add_commas/add_commas.cpp
@@ -0,0 +1,23 @@
+/*To make it easier for programmers to display numbers in this fashion,
+  implement a function, string addCommas(string digits);
+  that takes a string of decimal digits representing a number and returns the
+  string formed by inserting commas at every third position, starting on the
+  right
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
