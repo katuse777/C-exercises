01470566343a77ea6368af122c0b656ddd9ca1ef
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-22 11:37:50 +0200

    two cases have been covered for how to return the plural of a word, the cases covered are when the word ends with y and is preceded by a consonant or the word ends with either s,x or z

---
+++ plurals/regular_plurals.cpp
@@ -0,0 +1,53 @@
+/*Write a function createRegularPlural(word) that returns the plural of
+  word formed by following these standard English rules:
+  a. If the word ends in s, x, z, ch, or sh, add es to the word.
+  b. If the word ends in a y preceded by a consonant, change the y to ies.
+  c. In all other cases, add just an s.
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+std::string create_regular_plural(std::string word);
+bool preceded_by_consonant(std::string word);
+
+
+//MAIN FUNCTION
+int main()
+{
+  std::cout << create_regular_plural("fry") << '\n';
+
+  system("pause>0");
+  return 0;
+}
+
+std::string create_regular_plural(std::string word)
+{
+  char last_char = word[word.length() - 1];
+  if (last_char == 's' || last_char == 'x' || last_char == 'z')
+  {
+    word += "es";
+  }
+  else if (last_char == 'y' && preceded_by_consonant(word))
+  {
+    word.erase(word.length() - 1, 1);
+    word.insert(word.length(), "ies");
+  }
+  return word;
+}
+
+
+bool preceded_by_consonant(std::string word)
+{
+  char second_last_char = word[word.length() - 2];
+  bool is_a_consonant = true;
+  std::string vowels = "aeiou";
+  for (int i = 0; i < vowels.length(); i++)
+  {
+    if (second_last_char == vowels[i])  is_a_consonant = false;
+  }
+  return is_a_consonant;
+}
