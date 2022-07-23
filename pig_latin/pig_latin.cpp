cc3940b66feb1dffdb72dcbfca296a54dbbebbdd
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-23 09:35:10 +0200

    So far I've written 3 functions that are apply the rules for converting regular english to pig latin

--- pig_latin/pig_latin.cpp
+++ pig_latin/pig_latin.cpp
@@ -14,15 +14,71 @@
 
 
 //FUNCTION PROTOTYPES
+std::string to_pig_latin(std::string str);
+bool contains_no_vowels(std::string str);
+bool begins_with_consonant(std::string str);
+std::string consonants_until_first_vowel(std::string str);
 
 
-
-
+std::string vowels = "aeiou";
 //MAIN FUNCTION
 int main()
 {
 
-
     system("pause>0");
     return 0;
+}
+
+std::string to_pig_latin(std::string str)
+{
+
+}
+
+/*bool contains_vowels(std::string str)
+  --------------------------------------
+  this function determines whether the string passed in contains any vowels
+*/
+bool contains_no_vowels(std::string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < vowels.length(); j++)
+        {
+            if (str[i] == vowels[j]) return false;
+        }
+    }
+    return true;
+}
+
+/*bool begins_with_consonant(std::string str)
+  -------------------------------------------
+  this function determines whether the string passed in begins with a consonant
+*/
+bool begins_with_consonant(std::string str)
+{
+    for (int i = 0; i < vowels.length(); i++)
+    {
+        if (str[0] == vowels[i])    return false;
+    }
+    return true;
+}
+
+/*std::string consonants_until_first_vowel(std::string str)
+  ---------------------------------------------------------
+  this function will extract all the consonants before the first vowel in the string
+  provided the string begins with a consonant
+*/
+std::string consonants_until_first_vowel(std::string str)
+{
+    std::string chars_before_vowel = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < vowels.length(); j++)
+        {
+            if (!(str[i] == vowels[j]))
+                chars_before_vowel += str[i];
+            else return chars_before_vowel;    
+        }
+    }
+    return chars_before_vowel;
 }
