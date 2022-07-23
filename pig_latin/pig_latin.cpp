2cbc30d5e12a505bc07b007dfd234e3ee702fba9
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-23 08:54:14 +0200

    This program will read a string from the user and return that string as it would appear in pig latin

---
+++ pig_latin/pig_latin.cpp
@@ -0,0 +1,28 @@
+/*In Pig Latin, words are formed from their English counterparts by applying the following rules:
+  1. If the word contains no vowels, no translation is done, which means that the
+  translated word is the same as the original.
+  2. If the word begins with a vowel, the function adds the string "way" to the end
+  of the original word.
+  3. If the word begins with a consonant, the function extracts the string of
+  consonants up to the first vowel, moves that collection of consonants to the end
+  of the word, and adds the string "ay".
+*/
+
+#include <iostream>
+#include <string>
+#include <cctpye>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
