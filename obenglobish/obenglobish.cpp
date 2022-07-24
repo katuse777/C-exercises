6faaf227e2e3a9757a3ce5a79671bc227b3b9b10
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-24 08:24:54 +0200

    This program is a obenglobish translater

---
+++ obenglobish/obenglobish.cpp
@@ -0,0 +1,26 @@
+/*In official Obenglobish, the ob characters are added only before vowels
+  ob should be added before every vowel in the
+  English word except
+  • Vowels that follow other vowels
+  • An e that occurs at the end of the word
+  Write a function obenglobish that takes an English word and returns its
+  Obenglobish equivalent, using the translation rule given above. For example,
+  if you used your function with the main program
+*/
+
+#include <iostream>
+#include <string>
+
+
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
