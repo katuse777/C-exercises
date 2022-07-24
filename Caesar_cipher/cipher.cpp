2d9e26d506dafe3025cd3c4467d58da7995a7511
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-24 15:42:10 +0200

    This program is an implementation of the Caesar Cipher

---
+++ Caesar_cipher/cipher.cpp
@@ -0,0 +1,24 @@
+/*To implement a Caesar cipher, you should first define a function
+  string encodeCaesarCipher(string str, int shift);
+  that returns a new string formed by shifting every letter in str forward the
+  number of letters indicated by shift, cycling back to the beginning of the
+  alphabet if necessary
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    system("pause>0");
+    return 0;
+}
