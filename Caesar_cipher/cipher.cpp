9e560a9e1b2dbcb5476a7f3409b387df246e724a
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-25 16:19:57 +0200

    This program asks the user to enter a number for the computer to use to shift the letter in the string that the user is also asked to provide, for some reason when i try to enter a string the program stops executing and the encoding does not take place but if I enter the string directly into the function rather than through the console, it works perfectly fine

--- Caesar_cipher/cipher.cpp
+++ Caesar_cipher/cipher.cpp
@@ -1,4 +1,4 @@
-/*To implement a Caesar cipher, you should first define a function
+/**To implement a Caesar cipher, you should first define a function
   string encodeCaesarCipher(string str, int shift);
   that returns a new string formed by shifting every letter in str forward the
   number of letters indicated by shift, cycling back to the beginning of the
@@ -12,43 +12,82 @@
 
 //FUNCTION PROTOTYPES
 std::string encode_caesar_cipher(std::string str, int shift);
-char shift(char ch, int shift);
+char shift_characters(char ch, int shift);
 
 
+
+std::string user_string;
+int shift_val = 0;
 //MAIN FUNCTION
 int main()
 {
-    std::cout << shift('T', 13);
+	std::cout << "This program encodes a message using Caesar's Cipher.\n"
+		"Enter the number of character positions to shift: ";
+	std::cin >> shift_val;
+	std::cout << "Enter a message: ";
+    std::getline(std::cin, user_string);
+	std::cout << encode_caesar_cipher(user_string, shift_val);
 
-
-    system("pause>0");
-    return 0;
+	system("pause>0");
+	return 0;
 }
 
 
-/*char shift(char ch, int shift)
-  ------------------------------
-  this function takes in a character and a integer, converts the the character to it's
-  character code(an integer) and adds the integer (shift) to the character code and this
-  results in a new character code. If we are trying to shift a capital letter, then the new
-  letter should also be a capital letter, therefore no matter how big the shift is, the result 
-  must always be the character code of a capital letter in this case so after shifting the 
-  character code of the char passed in, we find the remainder when the new character code is divided
-  by the largest character code for the capital letter plus one, to ensure that the new character code
-  always falls in this range and we add the lowest character code of capital letters like so,
-  (ch_code % (highest_ch_code + 1)) + lowest_ch_code
-  the new character code will fall in the range: lowest_ch_code <= new_ch_code < highest_ch_code + 1
-  and I did this for all lower case letters too
+/*std::string encode_caesar_cipher(std::string str, int shift)
+* ------------------------------------------------------------
+  this function takes the string str and passes all it's individual charcters
+  into the shift_characters() function and uses the form a new string called encoded_msg,
+  this string is formed by shifting all the characters in str by the value of the shift parameter
+  and is the return value of this function
 */
-char shift(char ch, int shift)
+std::string encode_caesar_cipher(std::string str, int shift)
 {
-    int ch_code = ch;
-    ch_code += shift;
-    char shifted = ch_code;
-    if (isalpha(ch) && islower(ch))
-        shifted = (shifted % 123) + 96;
-    else
-        shifted = (shifted % 91) + 65;     
-    
-    return shifted;
+	std::string encoded_msg = "";
+	for (int i = 0; i < str.length(); i++)
+	{
+		encoded_msg += shift_characters(str[i], shift);
+	}
+	return encoded_msg;
+}
+
+
+
+/*char shift_characters(char ch, int shift)
+* -----------------------------------------
+  this function takes in a character (ch) and and integer (shift) and stores
+  ch in a integer called char_code so that we can perform a character shift 
+  by performing addition on the character code of (ch), if ch is a lower case
+  letter than to shift it by (shift) units we just add the value of shift to 
+  ch's character code (char_code) and if the character code is bigger than 122
+  which is the character code for the last lower case letter, we need the values
+  to circle back to the value of first character code for lower case letters,
+  thus we track by how many units the character code has overflown and add that 
+  number to the value of the first lower case character code and assign it to the 
+  (shifted_char) variable, and this is also done for uppercase letters
+*/
+char shift_characters(char ch, int shift)
+{
+	int char_code = ch;
+	wchar_t shifted_char = ' ';  // I'm using the wide char beacuse the normal char was too small to contain
+	int over_flow = 0;           //the values that i want to store in the shifted_char variable beacuse 
+	if (isalpha(ch) && islower(ch))
+	{
+		shifted_char = char_code + shift;
+		if (shifted_char > 122)
+		{
+			over_flow = shifted_char - 123;
+			shifted_char = 97 + over_flow;
+		}
+	}
+	else if (isalpha(ch) && isupper(ch))
+	{
+		shifted_char = char_code + shift;
+		if (shifted_char > 90)
+		{
+			over_flow = shifted_char - 91;
+			shifted_char = 65 + over_flow;
+
+		}
+	}
+	return shifted_char;
 }
