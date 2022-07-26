eead1f8a17f39ee9a513180efb93fd3096b932f4
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-26 07:50:57 +0200

    I've changed the shift_characters() function into a  more efficient version of it's self, no instead of using a switch statement with multiple cases, it uses a for loop and an if statement

--- letter-substitution_cipher/letter-substitution.cpp
+++ letter-substitution_cipher/letter-substitution.cpp
@@ -13,7 +13,7 @@ char shift_characters(char ch);
 std::string encode_message(std::string);
 
 std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-std::string substitution = "QWERTYUIOPASDFGHJKLZXCVBNM";
+std::string key = "QWERTYUIOPASDFGHJKLZXCVBNM";
 //MAIN FUNCTION
 int main()
 {
@@ -36,89 +36,10 @@ std::string encode_message(std::string str)
 
 char shift_characters(char ch)
 {
-    char new_char = ch;
-    switch(ch)
+    for (int i = 0; i < alphabet.length() ; i++)
     {
-        case 'A':
-            new_char = 'Q';
-            break;
-        case 'B':
-            new_char = 'W';
-            break;    
-        case 'C':
-            new_char = 'E';
-            break;
-        case 'D':
-            new_char = 'R';
-            break;    
-        case 'E':
-            new_char = 'T';
-            break;
-        case 'F':
-            new_char = 'Y';
-            break;
-        case 'G':
-            new_char = 'U';
-            break;
-        case 'H':
-            new_char = 'I';
-            break;
-        case 'I':
-            new_char = 'O';
-            break;
-        case 'J':
-            new_char = 'P';
-            break;
-        case 'K':
-            new_char = 'A';
-            break;
-        case 'L':
-            new_char = 'S';
-            break;
-        case 'M':
-            new_char = 'D';
-            break;
-        case 'N':
-            new_char = 'F';
-            break;
-        case 'O':
-            new_char = 'G';
-            break;
-        case 'P':
-            new_char = 'H';
-            break;
-        case 'Q':
-            new_char = 'J';
-            break;
-        case 'R':
-            new_char = 'K';
-            break;
-        case 'S':
-            new_char = 'L';
-            break;
-        case 'T':
-            new_char = 'Z';
-            break;
-         case 'U':
-            new_char = 'X';
-            break;
-        case 'V':
-            new_char = 'C';
-            break;
-        case 'W':
-            new_char = 'V';
-            break;
-        case 'X':
-            new_char = 'B';
-            break;
-        case 'Y':
-            new_char = 'N';
-            break;
-        case 'Z':
-            new_char = 'M';
-            break;                           
-        default:
-            new_char = ch;
+        if ( ch == alphabet[i] )
+            return key[i];
     }
-    return new_char;
+    return ch;
 }
