a88d5c15844e1f58f478679a77c5dd62b219caf7
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-28 19:14:35 +0200

    This program will print out a table of values of the sine and cosine of a set of angle values

---
+++ fstreams/formatted_trig_table/trig_table.cpp
@@ -0,0 +1,41 @@
+/*The <iomanip> library gives programmers more control over output format,
+  which makes it easy, for example, to create formatted tables. Write a program
+  that displays a table of trigonometric sines and cosines that looks like this:
+  The numeric columns should all be aligned on the right, and the columns
+  containing the trigonometric functions (which are listed here for angles in
+  degrees at 15-degree intervals) should all have seven digits after the decimal
+  point.
+            TrigTable
+  theta  | sin(theta) | cos(theta) |
+  -------+------------+------------+
+     -90 | -1.0000000 |  0.0000000 |
+     -75 | -0.9659258 |  0.2588190 |
+     -60 | -0.8660254 |  0.5000000 |
+     -45 | -0.7071068 |  0.7071068 |
+     -30 | -0.5000000 |  0.8660254 |
+     -15 | -0.2588190 |  0.9659258 |
+       0 |  0.0000000 |  1.0000000 |
+      15 |  0.2588190 |  0.9659258 |
+      30 |  0.5000000 |  0.8660254 |
+      45 |  0.7071068 |  0.7071068 |
+      60 |  0.8660254 |  0.5000000 |
+      75 |  0.9659258 |  0.2588190 |
+      90 |  1.0000000 |  0.0000000 |
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    return 0;
+}
