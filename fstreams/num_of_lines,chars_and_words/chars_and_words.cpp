a48170c87565d98aa26a94af2bd0de1fbc2689bf
Itumeleng Serala <seralakagiso88@gmail.com>
2022-07-31 13:39:04 +0200

    This program should be able to take in a file given by the user and report how many lines there are in the file, how mny words there are and how may characters there are

---
+++ fstreams/num_of_lines,chars_and_words/chars_and_words.cpp
@@ -0,0 +1,22 @@
+/*Write a program that reads a file and reports how many lines, words, and
+  characters appear in it. For the purposes of this program, a word consists of a
+  consecutive sequence of any characters except whitespace characters
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    return 0;
+}
