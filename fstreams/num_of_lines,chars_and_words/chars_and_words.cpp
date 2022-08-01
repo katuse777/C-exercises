0ed4c28919df184faaad6baa3e83fe12d827aef7
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-01 09:29:02 +0200

    The program is working as intented, if a existing file exists for input, the program is able to read the number of lines it that file, along with the number of characters and the number of words in said file

--- fstreams/num_of_lines,chars_and_words/chars_and_words.cpp
+++ fstreams/num_of_lines,chars_and_words/chars_and_words.cpp
@@ -30,6 +30,13 @@ int main()
 
     return 0;
 }
+
+
+/*void open_file(std::string filename)
+  ------------------------------------
+  this function opens the file entered by the user, if a function has processed the entire file via a loop
+  then the file has to be closed and opened up again if you want to process the file again from the start
+*/
 void open_file(std::string filename)
 {
     fin.open(filename.c_str());
@@ -40,6 +47,13 @@ void open_file(std::string filename)
     }
 }
 
+
+/*void get_file_from_user()
+  -------------------------
+  this function prints out a prompt to the user, asking them to enter the name of the file hey want to be read
+  and attempts to open that file and if the file could not be opened, the user will be prompted again this time
+  they'll know that they made a mistake and should try again
+*/
 void get_file_from_user()
 {
     do
@@ -51,6 +65,14 @@ void get_file_from_user()
     } while (fin.fail());
 }
 
+
+/*int num_of_lines()
+  ------------------
+  when this function is called, the input file has already been opened so, it is ready for processing
+  and when it's done, it will close the input file.
+  this function will get a line from the input file and increment the lines_counted variable until
+  the end of the file is reached and the return the value of the number of lines counted
+*/
 int num_of_lines()
 {
     std::string line_from_file;
@@ -64,6 +86,15 @@ int num_of_lines()
     return lines_counted;
 }
 
+
+
+/*int num_of_chars()
+  ------------------
+  this function calls the open_file() function
+  then loops through the file reading every char in the file and counting them,
+  once the the char that is being read is the EOF character, the function breaks
+  out of the loop, closes the file and returns the number of characters counted.
+*/
 int num_of_chars()
 {
     open_file(filename.c_str());
@@ -79,6 +110,19 @@ int num_of_chars()
     return chars_counted;
 }
 
+
+/*int num_of_words()
+  ------------------
+  this function calls the open_file() function
+  loop thorugh the file, counting the number of words.
+  This function read the characters in this file one-by-one
+  if the char that is read is not a space character, then 
+  the words_counted variable is incremented the function ignores
+  all other characters until it reads the next whitespace character
+  then will start looking for the next non-whitespace character and
+  increment the words_counted variable when the end of the file is 
+  reached, the file is closed and the function returns the number
+*/
 int num_of_words()
 {
    open_file(filename.c_str());
@@ -94,12 +138,20 @@ int num_of_words()
             ++words_counted;
             ignore_chars = true;
         }
-        else ignore_chars = false; 
+        else if (isspace(ch) && ignore_chars == true)  ignore_chars = false; 
    }
    fin.close();
    return words_counted;
 }
 
+
+/*void file_summary()
+  -------------------
+  this function calls the functions defined above, and uses those with a return value
+  to print out the return value next to a string the describes what that return value
+  represents, e.g
+  std::cout << "Number of lines counted: " << lines_counted << std::endl;
+*/
 void file_summary()
 {
     get_file_from_user();
