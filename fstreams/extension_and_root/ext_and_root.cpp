241e097cc9e152064f5974f8676193e5fc8dec1e
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-01 16:26:54 +0200

    This program is an implementation of two functions, one that takes in a file name and returns the root of the filename and the other returns the file extension

---
+++ fstreams/extension_and_root/ext_and_root.cpp
@@ -0,0 +1,27 @@
+/*The filelib.h interface exports several functions that make it easy to work
+  with filenames. Two functions that are particularly useful are getRoot and
+  getExtension, which divide a function into its root, which is the identifying
+  part of the filename, and the extension, which indicates its type. For example,
+  given the filename Middlemarch.txt used in the preceding exercise, the
+  root is Middlemarch and the extension is .txt (note that filelib.h defines
+  the extension to includes the dot). Write the code necessary to implement
+  these functions. To find out how to handle special cases, such as filenames
+  that don’t include a dot, consult the filelib.h interface in Appendix A.
+*/
+
+#include <iostream>
+#include <string>
+
+//FUNCTION PROTOTYPES
+
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    return 0;
+}
