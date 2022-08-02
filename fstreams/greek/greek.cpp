d903cba99f0e7d9503cc3d0e2c90a4d6069911a4
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-02 06:45:58 +0200

    This program will read characters from an input file and display them on the console after making the appropriate random substitutions

---
+++ fstreams/greek/greek.cpp
@@ -0,0 +1,23 @@
+/*Write a program that reads characters from an input file and displays them
+  on the console after making the appropriate random substitutions. Your
+  program should replace every uppercase character in the input by a random
+  uppercase character and every lowercase character by a random lowercase
+  one. Nonalphabetic characters should remain unchanged.
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    return 0;
+}
