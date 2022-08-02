e926cc45dfad58d9e24610358f8926f433c54b88
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-02 21:50:35 +0200

    This program will copy characters from input stream is to output stream os, execept for characters that appear inside c++ comments

---
+++ fstreams/ignore_comments/ignore_comments.cpp
@@ -0,0 +1,29 @@
+/*Write a function
+  void removeComments(istream & is, ostream & os);
+  that copies characters from the input stream is to the output stream os, except
+  for characters that appear inside C++ comments. Your implementation should
+  recognize both comment conventions:
+  • Any text beginning with / * and ending with * /, possibly many lines later.
+  • Any text beginning with //and extending through the end of the line
+*/
+
+#include <iostream>
+#include <string>
+#include <ios>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    return 0;
+}
