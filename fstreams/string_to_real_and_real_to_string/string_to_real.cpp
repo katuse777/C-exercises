778bfab0af6f5e190c3643e456979276a1b8c6c4
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-05 15:54:33 +0200

    This program contains an implementation of two functions called string_to_real() and string_to_real()

---
+++ fstreams/string_to_real_and_real_to_string/string_to_real.cpp
@@ -0,0 +1,44 @@
+/*int stringToInteger(string str)
+  {
+    istringstream stream(str);
+    int value;
+    stream >> value >> ws;
+    if (stream.fail() || !stream.eof())
+    {
+        error("stringToInteger: Illegal integer format");
+    }
+    return value;
+  }
+
+  string integerToString(int n) 
+  {
+    ostringstream stream;
+    stream << n;
+    return stream.str();
+  }
+  Using the functions stringToInteger and integerToString as a model,
+  write the code necessary to implement stringToReal and realToString.
+*/
+
+#include <iostream>
+#include <string>
+#include <istream>
+#include <ostream>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//GLOBAL VARIABLE DECLERATIONS
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+
+    return 0;
+}
