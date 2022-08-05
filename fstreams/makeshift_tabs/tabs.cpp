41d4ff97396d1ce3a68dc5e66b344f7ca3a1a7f3
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-05 12:13:32 +0200

    this is  program that copies an input file to an output file, replacing all tab   characters by the appropriate number of spaces

---
+++ fstreams/makeshift_tabs/tabs.cpp
@@ -0,0 +1,34 @@
+/*Some files use tab characters to align data into columns. Doing so, however,
+  can cause problems for applications that are unable to work directly with tabs.
+  For these applications, it is useful to have access to a program that replaces
+  tabs in an input file with the number of spaces required to reach the next tab
+  stop. In programming, tab stops are usually set at every eight columns. For
+  example, suppose that the input file contains a line of the form
+  abc-----|pqrst---|xyz
+  where the symbol -----| represents the space taken up by a tab, which differs
+  depending on its position in the line. If the tab stops are set every eight
+  spaces, the first tab character must be replaced by five spaces and the second
+  tab character by three.
+  Write a program that copies an input file to an output file, replacing all tab
+  characters by the appropriate number of spaces
+*/
+
+#include <iostream>
+#include <string>
+#include <istream>
+#include <ostream>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+     
+
+
+    return 0;
+}
+
