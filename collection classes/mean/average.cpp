ae1f07b1924423e014476ca40bf14c0c9ce6e77f
Itumeleng Serala <seralakagiso88@gmail.com>
2022-08-09 08:35:12 +0200

    This is a program that returns the mean of the data in the vector

---
+++ collection classes/mean/average.cpp
@@ -0,0 +1,26 @@
+/*In statistics, a collection of data values is often referred to as a distribution.
+  One of the primary goals of statistical analysis is to find ways to compress the
+  complete set of data into summary statistics that express properties of the
+  distribution as a whole. The most common statistical measure is the mean,
+  which is simply the traditional average. For the distribution x1, x2, x3, . . . , xn,
+  the mean is usually represented by the symbol .
+  Write a function
+    double mean(Vector<double> & data);
+  that returns the mean of the data in the vector
+*/
+
+#include <iostream>
+#include <vector>
+
+
+//FUNCTION PROTOTYPES
+
+
+
+//MAIN FUNCTION
+int main()
+{
+
+
+    return 0;
+}
